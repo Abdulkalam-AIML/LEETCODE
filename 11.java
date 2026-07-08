0487ba2907bdd3a427b3cf56eb4d5c3f3d3402ba
Syed Abdul Kalam <abdulkalamro20@gmail.com>
2026-07-08 15:58:26 +0530

    Implement maxArea method to calculate water container

---
+++ 11.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxArea(int[] height) {
+
+       int n=height.length;
+       int max=0;
+       int l=0;
+       int r=n-1;
+       while(l<r)
+       {
+        int h=Math.min(height[l] , height[r]);
+        int w= r-l;
+        int a =h*w;
+        max=Math.max(max,a);
+        if(height[l]<height[r])
+        {
+            l++;
+        }
+        else
+        {
+            r--;
+        }
+       }
+       return max;
+    }
+
+}
